033f187600693471ae72cb0325dd73adae53b22d
Darya Sharkova <darya.sharkova@jetbrains.com>
2022-07-25 11:41:18 +0200

    Filtering out bugfix-only releases.

--- lib/services/monitoring.rb
+++ lib/services/monitoring.rb
@@ -2,8 +2,12 @@ require 'octokit'
 
 module Services
   class Monitoring
+    ONLY_BUG_FIXES_PHRASES = ['bug fixes only', 'only bug fixes']
+    NEW_FEATURES_PHRASES = ['new features', 'features', 'added', 'enhancements']
+    BUG_FIXES = ['bug fixes', 'fixed']
     def self.set_repo_latest_release(repo, client)
       repo_path = repo.owner + '/' + repo.name
+      latest_release = nil
       begin
         releases = client.releases(repo_path)
       rescue Octokit::Error => e
@@ -35,7 +39,7 @@ module Services
         releases.each do |release|
           latest_release = release
           latest_tag = release.tag_name
-          unless latest_tag.include?("preview") || latest_tag.include?("rc") || latest_tag.include?("beta")
+          if important_tag? latest_tag
             break
           end
         end
@@ -43,7 +47,7 @@ module Services
         repo.latest_release_notes = latest_release.body
         latest_release_date = latest_release.published_at
       end
-      unless latest_tag.eql? repo.latest_tag || latest_tag.include?("preview") || latest_tag.include?("rc")
+      if latest_release.nil? ? important_tag?(latest_tag) : important_release?(latest_release)
         repo.read = false
         repo.latest_tag = latest_tag
         repo.latest_release_date = latest_release_date
@@ -57,6 +61,30 @@ module Services
       unless backtrace.nil?
         backtrace.each { |line| Rails.logger.error line }
       end
+    end
+
+    def self.important_release?(release)
+      unless important_tag? release.tag_name
+        return false
+      end
+
+      release_notes = release.body.downcase
+      if ONLY_BUG_FIXES_PHRASES.any? { |phrase| release_notes.include? phrase }
+        return false
+      end
+
+      sep = $/.nil? ? "\n" : $/
+      if BUG_FIXES.any? { |p| release_notes.include?('##' + p + sep) || release_notes.include?(p + ':' + sep) } &&
+        !NEW_FEATURES_PHRASES.any? { |p| release_notes.include? p }
+        return false
+      end
+
+      true
+    end
+
+    def self.important_tag?(tag)
+      tag = tag.downcase
+      !(tag.include?("preview") || tag.include?("rc") || tag.include?("beta"))
     end
   end
 end
